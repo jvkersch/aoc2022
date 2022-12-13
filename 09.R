5d1562acb86bb5c02e75b2f766ba3e5649ebd76c
Joris Vankerschaver <joris.vankerschaver@gmail.com>
2022-12-13 22:33:49 +0900

    Day 9, refactor

--- 09.R
+++ 09.R
@@ -4,21 +4,25 @@ disp_tl <- list(
   R=c(4,1,2,4,4,5,4,7,8),
   L=c(2,3,6,5,6,6,8,9,6),
   U=c(4,5,6,7,8,9,8,8,8),
-  D=c(2,2,2,1,2,3,4,5,6)
+  D=c(2,2,2,1,2,3,4,5,6),
+  UR=c(4,4,5,4,7,8,7,8,8),
+  DR=c(1,2,2,4,1,2,4,4,5),
+  UL=c(5,6,6,8,9,6,8,8,9),
+  DL=c(2,2,3,2,3,6,5,6,6)
 )
 disp_x <- list(R=1, L=-1, U=0, D=0)
 disp_y <- list(R=0, L=0, U=1, D=-1)
 offset_x <- c(-1, 0, 1, -1, 0, 1, -1, 0, 1)
 offset_y <- c(1, 1, 1, 0, 0, 0, -1, -1, -1)
 
-move_rope <- function(motions) {
+move_rope <- function(motions, tl_length = 1) {
   npos <- sum(map_int(motions, 2)) + 1
   all_pos <- vector(mode = "list", length = 10)
 
   i <- 1
   hd_x <- 0
   hd_y <- 0
-  tl <- 5
+  tl <- rep(5, tl_length)
   all_pos[[i]] <- c(hd_x, hd_y, tl)
   for (motion in motions) {
     direction <- motion[[1]]
@@ -34,6 +38,7 @@ move_rope <- function(motions) {
   
   all_pos
 }
+
 to_abs <- function(pos) {
   hd_x <- pos[[1]]
   hd_y <- pos[[2]]
